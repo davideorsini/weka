7840931b25db21d12113af13c5457432e210f9ff
Davide Orsini <Davide Orsini>
2016-07-13 14:47:26 +0200

    fourth commit

--- Manager.java
+++ Manager.java
@@ -10,31 +10,10 @@ import java.util.Random;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader.ArffReader;
 
-public class Manager{
-//	private ArrayList<Instance> instances;
-//	private ArrayList<Centroid> firstConfiguration;
-//	private ArrayList<Centroid> bestConfiguration;
-//	private ArrayList<Centroid> currentConfiguration;
-//	private int[][] combinationChecked;
-//	private int nClust;
-//	private String filePath;
-//	private int seed;
-//	private String distanceFunction;
-	
-	/*public Manager(int nClust, String filePath, int seed, String distanceFunction){
-		this.nClust = nClust;
-		this.filePath = filePath;
-		this.seed = seed;
-		this.distanceFunction = distanceFunction;
-	}*/
+public class Manager{	
 	
 	public static void main(String[] args) throws Exception{
-		//Manager manager = new Manager(Integer.parseInt(args[0]), args[1],
-		//								Integer.parseInt(args[2]), args[3]);
 		ArrayList<Instance> instances;
-		//ArrayList<Centroid> firstConfiguration;
-//		ArrayList<Centroid> bestConfiguration;
-//		ArrayList<Centroid> currentConfiguration;
 		Configuration currentConfig;
 		Configuration bestConfig = null;
 		int nClust = Integer.parseInt(args[0]);
@@ -78,17 +57,6 @@ public class Manager{
 		System.out.println(counter);
 	}
 	
-	/*public static ArrayList<Centroid> clone(ArrayList<Centroid> list){
-		ArrayList<Centroid> clone = new ArrayList<Centroid>();
-		for(int i=0; i<list.size(); i++){
-			Centroid c = new Centroid(list.get(i).getID());
-			c = list.get(i);
-			//Centroid c1 = c.c			
-		}
-		clone = (ArrayList<Centroid>) list.clone();
-		return clone;
-	}*/
-	
 	public static void printStatus(ArrayList<Centroid> cluster){
 		System.out.print("Centroids: ");
 		for(int i=0; i<cluster.size(); i++){
@@ -96,10 +64,4 @@ public class Manager{
 		}
 	}
 	
-	
-	
-	
-	public void checkCombinations(){
-		
-	}
 }
